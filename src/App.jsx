36b67dd6d530e39c2da1bd20422992af09ebe796
Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2023-10-06 07:01:34 +0600

    donee

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,28 @@ function TabContent({ item }) {
     setLikes(likes + 1);
   }
 
+  function handleTripleInc() {
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+
+    setLikes((l) => l + 1);
+    setLikes((l) => l + 1);
+    setLikes((l) => l + 1);
+  } 
+
+  function handleUndo(){
+    setShowDetails(true)
+    setLikes(0)
+  }
+
+  function handleUndoIn2s(){
+    // setTimeout(handleUndo, 2000)
+    setTimeout(() => {
+      setShowDetails(true)
+      setLikes(0)
+    }, 2000)
+  }
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -79,13 +101,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoIn2s}>Undo in 2s</button>
       </div>
     </div>
   );
